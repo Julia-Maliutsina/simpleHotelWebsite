6d0bcd9fc9ddbff6591d75211960ba3872fc0525
Julia-Maliutsina <89554520+Julia-Maliutsina@users.noreply.github.com>
2021-08-26 03:31:21 +0300

    Create load_article.js

---
+++ load_article.js
@@ -0,0 +1,9 @@
+    function loadArticle1() {
+        $('#article1').load("articles_blog/article1.html");
+    }
+    function loadArticle2() {
+        $('#article2').load("articles_blog/article2.html");
+    }
+    function loadArticle3() {
+        $('#article3').load("articles_blog/article3.html");
+    }
